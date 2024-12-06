4725615e9011ac8d80dc9dec82365d8a6f157701
berur4 <berurcaleb@gmail.com>
2024-12-06 08:35:11 +0300

    fetch crud lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
+  function handleDeleteQuestion(id) {
+    setQuestions(questions.filter((question) => question.id !== id));
+  }
+
+  function handleUpdateQuestion(updatedQuestion) {
+    setQuestions(
+      questions.map((question) =>
+        question.id === updatedQuestion.id ? updatedQuestion : question
+      )
+    );
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDeleteQuestion={handleDeleteQuestion}
+            onUpdateQuestion={handleUpdateQuestion}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
